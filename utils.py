af99cdacb1f5e52e75b00f869b1de2100f997cce
Jerome Onwunalu <jeronwunalu@gmail.com>
2022-04-17 10:58:42 -0500

    Impute X_test to remove missing data

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
-import pandas as p d
+import pandas as pd
 import numpy as np
+from sklearn.experimental import enable_iterative_imputer
 from sklearn import model_selection, preprocessing, impute
 
 def tweak_titanic(df):
-    df = df.drop(columns=["name", "ticket", "home.dest", "boat", "body", "cabin"]).pip(pd.get_dummies,drop_first=True)
+    df = df.drop(columns=["name", "ticket", "home.dest", "boat", "body", "cabin"]).pipe(pd.get_dummies,drop_first=True)
     return df
 
 def get_train_test_X_y(df, y_col, size=0.3, std_cols=None):
@@ -13,8 +14,10 @@ def get_train_test_X_y(df, y_col, size=0.3, std_cols=None):
     cols = X.columns
     num_cols = ["pclass", "age", "sibsp", "parch", "fare"]
     fi = impute.IterativeImputer()
+    # Impute the X_train data with fit_transform()
     X_train.loc[:, num_cols] = fi.fit_transform(X_train[num_cols])
-
+    # Imput the X_test data with transform()
+    X_test.loc[:, num_cols]  = fi.transform(X_test[num_cols])
     if std_cols:
         std = preprocessing.StandardScaler()
         X_train.loc[:, std_cols] = std.fit_transform(X_train[std_cols])
